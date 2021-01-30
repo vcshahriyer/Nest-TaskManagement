da4955bbae3f627cff7f2ec2bf9a7ec3855b0fd2
Raghib Shahriyer <shahriyer@elobyte.com>
2021-01-30 20:43:35 +0600

    Task validation Pipe added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,13 +15,14 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTasksWithFilters(filterDto);
     } else {
@@ -40,7 +41,10 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  updateTaskById(@Param('id') id: string, @Body('status') status: TaskStatus) {
+  updateTaskById(
+    @Param('id') id: string,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ) {
     return this.tasksService.updateTaskById(id, status);
   }
 
